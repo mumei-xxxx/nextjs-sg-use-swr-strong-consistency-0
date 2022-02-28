b7d1bbbfe56dc8e8ffd9ea03fd4de1e3a8da546a
mumei <lesilluminations0@gmail.com>
2022-02-28 14:13:17 +0900

    定期的な再検証をし、自動更新する。

--- src/infrastructure/hooks/useBitcoinDataSWR.ts
+++ src/infrastructure/hooks/useBitcoinDataSWR.ts
@@ -7,10 +7,12 @@ import { fetcher } from '@/libraries/fetcher'
  * 参考にしたコード
  * useSWR api-hooks サンプル
  * https://github.com/vercel/swr/tree/main/examples/api-hooks
+ * SWR ― 定期的な再検証
+ * https://swr.vercel.app/ja/docs/revalidation#%E5%AE%9A%E6%9C%9F%E7%9A%84%E3%81%AA%E5%86%8D%E6%A4%9C%E8%A8%BC
  */
 export const useBitcoinDataSWR = (
   fallbackData: BitflyerTickerResponseType
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): SWRResponse<BitflyerTickerResponseType, any> => {
-  return useSWR(`api/bitcoin`, fetcher, { fallbackData })
+  return useSWR(`api/bitcoin`, fetcher, { fallbackData, refreshInterval: 10000 })
 }
